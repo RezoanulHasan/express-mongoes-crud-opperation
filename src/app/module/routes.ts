e944c28ef0c376f43338b55f5ab5270517613982
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-11-23 04:34:30 +0600

    add routes for orders

--- src/app/module/routes.ts
+++ src/app/module/routes.ts
@@ -8,5 +8,10 @@ router.get('/api/users', UserController.getAllUsers);
 router.get('/api/users/:userId', UserController.getUserById);
 router.put('/api/users/:userId', UserController.updateUser);
 router.delete('/api/users/:userId', UserController.deleteUser);
-
+router.put('/api/users/:userId/orders', UserController.addOrder);
+router.get('/api/users/:userId/orders', UserController.getOrders);
+router.get(
+  '/api/users/:userId/orders/total-price',
+  UserController.calculateTotalPrice,
+);
 export default router;
