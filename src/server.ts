4422e9d36c2b98b5c4ac53a75f3be18e984413b6
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-11-23 10:26:58 +0600

    update  index and server file

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import config from './app/config';
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-
     app.listen(config.port, () => {
       console.log(`app is listening on port ${config.port}`);
     });
